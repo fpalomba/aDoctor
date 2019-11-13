20881a1598b2878a8886a3281331815ff20eec23
jean <jeancs.patricio@gmail.com>
2019-11-13 17:07:42 -0300

    Ignoring constructors for MIM checking.

--- aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/MemberIgnoringMethodRule.java
+++ aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/MemberIgnoringMethodRule.java
@@ -6,8 +6,11 @@ import it.unisa.aDoctor.beans.MethodBean;
 public class MemberIgnoringMethodRule {
 
     public boolean isMemberIgnoringMethod(ClassBean pClass) {
-
         for (MethodBean methodBean : pClass.getMethods()) {
+            if (methodBean.getTextContent().contains(pClass.getName())) {
+                continue;
+            }
+
             if (!methodBean.isStatic()) {
                 if (pClass.getInstanceVariables().size() > 0) {
                     if (methodBean.getUsedInstanceVariables().isEmpty()) {
